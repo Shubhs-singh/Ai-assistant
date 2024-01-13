f093644d67d76eafd28e55119be6a989b82599b1
Shubhs-singh <shubhamrsingh08@gmail.com>
2024-01-13 16:57:57 +0530

    adding first file

---
+++ main.py
@@ -0,0 +1,25 @@
+import os 
+import pygame
+
+def speak(text):
+    voice ="en-US-AriaNeural"
+    
+    command= f'edge-tts --voice "{voice}" --text "{text}" --write-media "output.mp3"'
+    
+    os.system(command)
+    pygame.mixer.init()
+    pygame.mixer.init()
+    
+    try:
+        pygame.mixer.music.load("output.mp3")
+        
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+    except Exception as e:
+        print(e)
+    finally:
+        pygame.mixer.music.stop()
+        pygame.mixer.quit()
+
+speak(" hello how are you shubham you are great")
