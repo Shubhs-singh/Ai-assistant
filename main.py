c4f0c80040d1cfd28e9358bc46ec1a230eb95b30
Shubhs-singh <shubhamrsingh08@gmail.com>
2024-01-13 17:57:07 +0530

    added feature so that our model can take voice input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os 
 import pygame
+import speech_recognition as sr
 
 def speak(text):
     voice ="en-US-AriaNeural"
@@ -22,4 +23,30 @@ def speak(text):
         pygame.mixer.music.stop()
         pygame.mixer.quit()
 
+def take_command():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listing .....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try :
+        print('Recognizing')
+        query = r.recognize_google(audio, language ='en-us')
+    except Exception as e:
+        print(e)
+        return ''
+    return query
 speak(" hello how are you shubham you are great")
+query = take_command()
+print(query)
+
+while True:
+    query = take_command()
+    print("You:" + query)
+    if ' hello' in query or 'hey' in query:
+        speak("hi how are you")
+    elif ' i am fine ' in query:
+        speak('thats great to hear')
+    else :
+        speak(' i dont understand ') 
+    
